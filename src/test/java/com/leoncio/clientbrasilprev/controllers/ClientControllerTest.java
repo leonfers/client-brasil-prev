0b88760cc0d5bb851560048bbb5c3a7b339504e3
Leoncio Ferreira Da Silva Neto <leonfers@hotmail.com>
2021-07-07 10:04:46 -0300

    feat: adicao de teste de listagem de clientes (#3)

---
+++ src/test/java/com/leoncio/clientbrasilprev/controllers/ClientControllerTest.java
@@ -0,0 +1,65 @@
+package com.leoncio.clientbrasilprev.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leoncio.clientbrasilprev.dtos.ClientDTO;
+import com.leoncio.clientbrasilprev.models.Client;
+import com.leoncio.clientbrasilprev.services.ClientService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles({ "test"})
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
+class ClientControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ClientService clientService;
+
+
+    @Test
+    void it_should_return_list_of_clients_dto() throws Exception {
+        List<ClientDTO> clients = new ArrayList<>();
+        ClientDTO client = new ClientDTO();
+        client.setId(1L);
+        client.setName("Cliente de Teste");
+        client.setCpf("00901903000");
+        client.setNumber("000");
+        client.setStreet("Rua de Teste");
+        client.setCity("Cidade Teste");
+        client.setUf("PI");
+        client.setZipCode("6400000");
+
+        clients.add(client);
+
+        when(clientService.findAll()).thenReturn(clients);
+        mockMvc.perform(MockMvcRequestBuilders.get("/clients")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data[0].id").value(client.getId()));
+    }
+
+}
