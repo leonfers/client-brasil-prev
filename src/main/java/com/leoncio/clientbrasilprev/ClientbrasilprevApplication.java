c40c17c754d6b83810e8431d203a163e01bd6146
lyonfers <leonfers@hotmail.com>
2021-07-07 01:02:31 -0300

    feat: inicializacao do projeto

---
+++ src/main/java/com/leoncio/clientbrasilprev/ClientbrasilprevApplication.java
@@ -0,0 +1,13 @@
+package com.leoncio.clientbrasilprev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientbrasilprevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientbrasilprevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leoncio/clientbrasilprev/ClientbrasilprevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoncio.clientbrasilprev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientbrasilprevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
